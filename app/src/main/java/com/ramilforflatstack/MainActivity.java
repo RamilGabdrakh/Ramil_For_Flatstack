e6824f1f53bcf85ecc43de066fd17164aafa8bd2
Gabdrakhmanov Ramil <gramilwow@gmail.com>
2015-06-14 13:59:43 +0300

    add vk sdk

--- app/src/main/java/com/ramilforflatstack/MainActivity.java
+++ app/src/main/java/com/ramilforflatstack/MainActivity.java
@@ -12,6 +12,8 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.act_main);
+
+        String[] fingerprints = VKUtil.getCertificateFingerprint(this, this.getPackageName());
     }
 
 
