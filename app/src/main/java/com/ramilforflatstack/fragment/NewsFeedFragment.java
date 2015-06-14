59e22ec9ab70993a9aae98069cc3b2ba22c0d83e
Gabdrakhmanov Ramil <gramilwow@gmail.com>
2015-06-14 18:47:23 +0300

    create news feed fragment

--- app/src/main/java/com/ramilforflatstack/fragment/NewsFeedFragment.java
+++ app/src/main/java/com/ramilforflatstack/fragment/NewsFeedFragment.java
@@ -1,10 +1,22 @@
 package com.ramilforflatstack.fragment;
 
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.ramilforflatstack.R;
 
 /**
  * Created by Ramil on 14.06.2015.
  */
 public class NewsFeedFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fmt_news_feed, container, false);
+
+        return  view;
+    }
 }
