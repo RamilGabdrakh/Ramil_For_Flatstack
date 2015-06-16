c6c310edf60714c1a572cf35dd98f93a2611fcce
Ramil Gabdrakhmanov <ramil.gabdrakhmanov@dz.ru>
2015-06-16 16:23:10 +0300

    add toolbar

--- app/src/main/java/com/ramilforflatstack/activity/NewsFeedActivity.java
+++ app/src/main/java/com/ramilforflatstack/activity/NewsFeedActivity.java
@@ -3,14 +3,21 @@ package com.ramilforflatstack.activity;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import com.ramilforflatstack.R;
 import com.ramilforflatstack.fragment.NewsFeedFragment;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 /**
  * Created by Ramil on 14.06.2015.
  */
 public class NewsFeedActivity extends VkActivity {
+
+    @InjectView(R.id.toolbar)
+    Toolbar mToolbar;
 
     public static void start(Activity activity) {
         activity.startActivity(new Intent(activity, NewsFeedActivity.class));
@@ -20,6 +27,10 @@ public class NewsFeedActivity extends VkActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.act_news_feed);
+        ButterKnife.inject(this);
+
+        setSupportActionBar(mToolbar);
+        mToolbar.setNavigationIcon(R.drawable.ic_close_white_36dp);
 
         getSupportFragmentManager().beginTransaction()
                 .replace(R.id.container, new NewsFeedFragment(), NewsFeedFragment.class.getName())
